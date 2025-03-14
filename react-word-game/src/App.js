3bc384e3c87ab62eda0ca01fa09d859b5914d989
Abderrahman Abaakil <abderrahmanabaakil@MBPdeAberrahman.lan>
2025-03-15 00:04:28 +0100

    Enter key handling for current guess input

--- react-word-game/src/App.js
+++ react-word-game/src/App.js
@@ -6,6 +6,43 @@ const GUESS_LENGTH = 5;
 function App() {
   const [solution, setSolution] = useState("");
   const [guesses, setGuesses] = useState(Array(6).fill(null));
+  const[currentGuess, setCurrentGuess] = useState('');
+  const[isGameOver, setIsgameOver] = useState(false);
+
+  useEffect(()=> {
+   const handleType = (event)=> {
+    if(isGameOver){
+      return;
+    }
+    
+
+
+    if(event.key === 'Enter'){
+       if(currentGuess.length !=5){
+        return;
+       }
+       const isCorrect = solution === currentGuess;
+       if(isCorrect){
+        setIsgameOver(true);
+       }
+    }
+
+    if(event.key ==='Backspace')
+    {
+      setCurrentGuess(currentGuess.slice(0,-1));
+      return;
+    }
+
+    if( currentGuess.length >=5)
+    {
+      return;
+    }
+    setCurrentGuess(oldGuess => oldGuess + event.key);
+   };  
+
+  window.addEventListener('keydown', handleType);
+  return () => window.removeEventListener('keydown',handleType);
+  },[currentGuess, isGameOver, solution]);
 
   useEffect(() => {
     const fetchWord = async () => {
@@ -23,9 +60,10 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      {guesses.map(guess=>{
-        return <Line guess = {guess ?? ''}/>
+    <div className="board">
+      {guesses.map((guess, i)=>{
+        const isCurrentGuess = i === guesses.findIndex(val => val == null);
+        return <Line guess = {isCurrentGuess? currentGuess: guess ?? ''}/>
       })}
     </div>
   );
