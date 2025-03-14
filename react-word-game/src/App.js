c78102aefa65f8d35a9c73e7c64c5e9fd077df7e
Abderrahman Abaakil <abderrahmanabaakil@MBPdeAberrahman.lan>
2025-03-14 23:11:15 +0100

    Add an algo to fetch a random word and a proxy to bypass CORS

--- react-word-game/src/App.js
+++ react-word-game/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
+const API_URL = "/api/fe/wordle-words";
 function App() {
+  const [solution, setSolution] = useState("");
+
+  useEffect(() => {
+    const fetchWord = async () => {
+      try {
+        const response = await fetch(API_URL);
+        const words = await response.json();
+        const randomWord = words[Math.floor(Math.random() * words.length)];
+        setSolution(randomWord);
+      } catch (error) {
+        console.error("Error fetching words:", error);
+      }
+    };
+
+    fetchWord();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Local</h1>
+      <h2>{solution}</h2>
     </div>
   );
-}
+  }
 
 export default App;
