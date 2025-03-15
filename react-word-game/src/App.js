d5994a80702cdacff5c222bbd0af6d9af3aa896d
Abderrahman Abaakil <abderrahmanabaakil@MBPdeAberrahman.lan>
2025-03-15 17:37:22 +0100

    Update the fetch for the deployement

--- react-word-game/src/App.js
+++ react-word-game/src/App.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const API_URL = "/api/fe/wordle-words";
+//const API_URL = "/api/fe/wordle-words";
+const API_URL ="https://api.frontendexpert.io/api/fe/wordle-words";
 const GUESS_LENGTH = 5;
 
 function App() {
